d738a9c99069f31c00789ecd9145d53280c919ab
HarishBalaji ShanmugaSundaram <harishbalajishanmugasundaram@gmail.com>
2020-05-04 22:36:20 +0200

    Initial Commit

---
+++ Project Euler/Multiples of 3 and 5/index.js
@@ -0,0 +1,12 @@
+function multiplesOf3and5(number) {
+    // Good luck!
+    var sum = 0;
+    for (var i = 3; i < number; i++) {
+        if ((i % 3 == 0) || (i % 5 == 0)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+console.log(multiplesOf3and5(10));
